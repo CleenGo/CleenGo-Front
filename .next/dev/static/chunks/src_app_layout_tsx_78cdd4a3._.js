518b5942d829b055a0cc4b062032020bef5804bf
AMorenoPortas <quarentienda2@gmail.com>
2025-11-30 23:55:21 -0300

    corregimos navbar

--- .next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
@@ -2,8 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_app_globals_91e4631d.css",
-  "static/chunks/node_modules_next_85bcaee2._.js",
-  "static/chunks/src_app_c189daac._.js"
+  "static/chunks/_3f09c48a._.js"
 ],
     source: "dynamic"
 });
