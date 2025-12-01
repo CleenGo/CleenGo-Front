afc2a41f6f3f57d2eeb1f35959142be676da5e51
PauloCLG09 <pauce01@gmail.com>
2025-12-01 01:38:18 -0500

    correcciones

--- .next/dev/static/chunks/src_app_login_page_tsx_808d0be7._.js
+++ .next/dev/static/chunks/src_app_login_page_tsx_808d0be7._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_c17cde37._.js",
+  "static/chunks/node_modules_607ba0ff._.js",
   "static/chunks/src_app_e33141b6._.js"
 ],
     source: "dynamic"
