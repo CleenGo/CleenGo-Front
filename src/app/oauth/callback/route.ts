dbc10b13445d2daacbf4686092773ae345dc086f
PauloCLG09 <pauce01@gmail.com>
2025-12-03 23:55:19 -0500

    correccion

--- src/app/oauth/callback/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
-
-export function GET() {
-  return new Response("OAuth callback route active.");
-}
